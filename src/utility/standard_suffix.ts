5604da65995a69908358438f169ba9b69b5f6a30
William Yang <williamyang2017@gmail.com>
2025-06-24 18:06:15 +1000

    Make SI prefix sign invariant and handle zeros

--- src/utility/standard_suffix.ts
+++ src/utility/standard_suffix.ts
@@ -3,7 +3,12 @@ export interface StandardSuffix {
   suffix: string;
 }
 
-export function get_standard_suffix(value: number): StandardSuffix {
+export function get_standard_suffix(value: number, threshold?: number): StandardSuffix {
+  // suffix doesn't care about sign
+  value = Math.abs(value);
+  if (threshold !== undefined && value <= threshold) {
+    return { scale: 1, suffix: "" };
+  }
   if (value >= 1e9) return { scale: 1e9, suffix: "G" };
   if (value >= 1e6) return { scale: 1e6, suffix: "M" };
   if (value >= 1e3) return { scale: 1e3, suffix: "k" };
@@ -11,11 +16,12 @@ export function get_standard_suffix(value: number): StandardSuffix {
   if (value >= 1e-3) return { scale: 1e-3, suffix: "m" };
   if (value >= 1e-6) return { scale: 1e-6, suffix: "µ" };
   if (value >= 1e-9) return { scale: 1e-9, suffix: "n" };
-  return { scale: 1e-12, suffix: "f" };
+  if (value >= 1e-12) return { scale: 1e-12, suffix: "f" };
+  return { scale: 1, suffix: "" };
 }
 
-export function with_standard_suffix(value: number, unit?: string, precision?: number): string {
+export function with_standard_suffix(value: number, unit?: string, precision?: number, threshold?: number): string {
   precision = precision ?? 3;
-  const result = get_standard_suffix(value);
+  const result = get_standard_suffix(value, threshold);
   return `${(value/result.scale).toPrecision(precision)} ${result.suffix}${unit ?? ''}`;
 }
