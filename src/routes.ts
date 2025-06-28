c0db3484d247efdf15ecccc746eb4a24c789d668
William Yang <williamyang2017@gmail.com>
2025-06-29 04:00:36 +1000

    Use dynamic imports to reduce page size

--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,6 @@ import type { FunctionalComponent } from 'vue';
 import { type RouteComponent } from 'vue-router';
 import { CuboidIcon, PencilIcon, Grid2x2Icon } from 'lucide-vue-next';
 
-import { App3DView } from './views/app_3d';
-import Stackup2DEditor from './views/stackup_2d/StackupRefactor.vue';
-import Stackup2DTemplates from './views/stackup_2d/TemplatesView.vue';
-
 export interface CustomRoute {
   name: string;
   path: string;
@@ -17,19 +13,19 @@ export const routes: CustomRoute[] = [
   {
     name: "3D Simulator",
     path: "/3d_calculator",
-    view_component: App3DView,
+    view_component: () => import("./views/app_3d/App3DView.vue"),
     icon_component: CuboidIcon,
   },
   {
     name: "Stackup 2D",
     path: "/stackup_2d/editor",
-    view_component: Stackup2DEditor,
+    view_component: () => import("./views/stackup_2d/StackupRefactor.vue"),
     icon_component: PencilIcon,
   },
   {
     name: "Stackup 2D Templates",
     path: "/stackup_2d/templates",
-    view_component: Stackup2DTemplates,
+    view_component: () => import("./views/stackup_2d/TemplatesView.vue"),
     icon_component: Grid2x2Icon,
   },
 ];
