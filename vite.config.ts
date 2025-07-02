0f4ea6eedc108d12c0915c2941fd85253ecc58f5
William Yang <williamyang2017@gmail.com>
2025-07-02 12:30:45 +1000

    Added github pages deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite';
 import svgLoader from 'vite-svg-loader';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-    svgLoader(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()) as Partial<Record<string, string>>;
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      tailwindcss(),
+      svgLoader(),
+    ],
+    base: env.VITE_BASE_URL,
+  }
 })
