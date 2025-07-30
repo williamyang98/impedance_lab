35fa666d67efdc74febd37c26af1816eb2f23505
William Yang <williamyang2017@gmail.com>
2025-07-30 23:54:34 +1000

    Dont show energy from simulation

--- src/views/via_2d/ImpedanceResultView.vue
+++ src/views/via_2d/ImpedanceResultView.vue
@@ -25,14 +25,14 @@ const _props = defineProps<{
       <td class="font-medium">Inductance</td>
       <td>{{ with_standard_suffix(result.Lh, "H", 4) }}</td>
     </tr>
-    <tr>
+    <!-- <tr>
       <td class="font-medium">Energy homogenous</td>
       <td>{{ with_standard_suffix(result.energy_homogenous, "J", 4) }}</td>
     </tr>
     <tr>
       <td class="font-medium">Energy inhomogenous</td>
       <td>{{ with_standard_suffix(result.energy_inhomogenous, "J", 4) }}</td>
-    </tr>
+    </tr> -->
     <tr>
       <td class="font-medium">Propagation Delay</td>
       <td>{{ with_standard_suffix(result.propagation_delay, "s", 4) }}</td>
