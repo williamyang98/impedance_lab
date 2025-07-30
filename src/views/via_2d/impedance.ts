2b192b59c285bf72a93b7efdb11be82a01963793
William Yang <williamyang2017@gmail.com>
2025-07-30 23:35:58 +1000

    Switch between different via inductance models based on ratio of inner/outer diameter for infinitely hollow, tubular and solid vias

--- src/views/via_2d/impedance.ts
+++ src/views/via_2d/impedance.ts
@@ -1,5 +1,5 @@
 import { Profiler } from "../../utility/profiler.ts";
-import type { StackupGrid } from "./stackup_to_grid.ts";
+import type { StackupGrid, ViaBarrelInfo } from "./stackup_to_grid.ts";
 
 export interface ImpedanceResult {
   voltage: number;
@@ -16,8 +16,77 @@ export interface ImpedanceResult {
   dc_resistance: number;
 }
 
-export function calculate_via_impedance(stackup_grid: StackupGrid, profiler?: Profiler): ImpedanceResult {
+export type InductanceMethod =
+  "solid_fw_grover" |
+  "solid_goldfarb_pucel" |
+  "tube_dc_vt_morgan" |
+  "solid_wu" |
+  "tube_sapongin_prokopenko" |
+  "tube_thin_sapongin_prokopenko";
 
+export function calculate_via_inductance(via: ViaBarrelInfo, method: InductanceMethod): number {
+  const mu_0 = Math.PI*4e-7;
+  switch (method) {
+    case "solid_fw_grover": {
+      // https://www.allaboutcircuits.com/tools/wire-self-inductance-calculator/
+      const h = via.height*100; // cm
+      const d = via.outer_diameter*100; // cm
+      const a0 = d/(2*h);
+      const a1 = Math.sqrt(1+a0*a0);
+      return 2*h*(Math.log((1+a1)/a0) - a1 + mu_0/4 + a0)*1e-9;
+    }
+    case "solid_goldfarb_pucel": {
+      // https://resources.system-analysis.cadence.com/blog/msa2021-is-there-a-via-inductance-rule-of-thumb
+      const h = via.height;
+      const r = via.outer_diameter/2;
+      const a0 = Math.sqrt(r**2 + h**2);
+      return mu_0/(2*Math.PI)*(h*Math.log((h+a0)/r) + 1.5*(r-a0));
+    }
+    case "tube_dc_vt_morgan": {
+      // https://upcommons.upc.edu/server/api/core/bitstreams/928a71c0-b0a3-49ad-bf73-6f7a02ad2609/content
+      // Equation 11 - DC partial self inductance (How is DC involved in inductance???)
+      const h = via.height;
+      const r1 = via.inner_diameter/2;
+      const r2 = via.outer_diameter/2;
+      const a0 = r2**2 - r1**2;
+      return h*mu_0/(2*Math.PI)*((r1**4)/(a0**2)*Math.log(r2/r1) + (3*r1**2 - r2**2)/(4*a0));
+    }
+    case "solid_wu": {
+      // https://upcommons.upc.edu/server/api/core/bitstreams/928a71c0-b0a3-49ad-bf73-6f7a02ad2609/content
+      // Equation 9 - Solid cylindrical conductor (Wu et al)
+      const h = via.height;
+      const r = via.outer_diameter/2;
+      const a0 = Math.sqrt(1 + h**2/r**2);
+      const a1 = Math.sqrt(1 + r**2/h**2);
+      return h*mu_0/(2*Math.PI)*(Math.log(1/r+a0) - a1 + r/h + 1/4);
+    }
+    case "tube_sapongin_prokopenko": {
+      // https://upcommons.upc.edu/server/api/core/bitstreams/928a71c0-b0a3-49ad-bf73-6f7a02ad2609/content
+      // Equation 13 - Infinitely long tubular conductors (Sapongin and Prokopenko)
+      const h = via.height;
+      const r1 = via.inner_diameter/2;
+      const r2 = via.outer_diameter/2;
+      const a0 = r1/r2;
+      const a1 = a0**2;
+      const R = Math.max(h,r1,r2)*10; // some wierd virtual cylinder we need???
+      return h*mu_0/(4*Math.PI)*((1-a1+2*a1*Math.log(a0))/(1-a1) + 2*Math.log(R/r2));
+    }
+    case "tube_thin_sapongin_prokopenko": {
+      // https://upcommons.upc.edu/server/api/core/bitstreams/928a71c0-b0a3-49ad-bf73-6f7a02ad2609/content
+      // Equation 14 - Infinitely long thin-wall tubular conductors (Sapongin and Prokopenko)
+      const h = via.height;
+      const r1 = via.inner_diameter/2;
+      const r2 = via.outer_diameter/2;
+      const R = Math.max(h,r1,r2)*10; // some wierd virtual cylinder we need???
+      const t = (r2-r1)/r2;
+      const t2 = t*t;
+      const t4 = t2*t2;
+      return h*mu_0/(4*Math.PI)*(t + t2/6 - t4/30 + 2*Math.log(R/r2));
+    }
+  }
+}
+
+export function calculate_via_impedance(stackup_grid: StackupGrid, profiler?: Profiler): ImpedanceResult {
   const grid = stackup_grid.grid;
 
   profiler?.begin("bake");
@@ -64,65 +133,17 @@ export function calculate_via_impedance(stackup_grid: StackupGrid, profiler?: Pr
 
   // we only consider the inductance caused by the barrel part of the inductor
   const via = stackup_grid.via_barrel_parameters;
-  const calculate_inductance = (mode: 0 | 1 | 2 | 3 | 4 | 5): number => {
-    const mu_0 = Math.PI*4e-7;
-    const c_0 = 3e8;
-    switch (mode) {
-      case 0: {
-        // https://www.allaboutcircuits.com/tools/wire-self-inductance-calculator/
-        const h = via.height*100; // cm
-        const d = via.outer_diameter*100; // cm
-        const a0 = d/(2*h);
-        const a1 = Math.sqrt(1+a0*a0);
-        return 2*h*(Math.log((1+a1)/a0) - a1 + mu_0/4 + a0)*1e-9;
-      }
-      case 1: {
-        // https://resources.system-analysis.cadence.com/blog/msa2021-is-there-a-via-inductance-rule-of-thumb
-        const h = via.height;
-        const r = via.outer_diameter/2;
-        const a0 = Math.sqrt(r**2 + h**2);
-        return mu_0/(2*Math.PI)*(h*Math.log((h+a0)/r) + 1.5*(r-a0));
-      }
-      case 2: {
-        // https://upcommons.upc.edu/server/api/core/bitstreams/928a71c0-b0a3-49ad-bf73-6f7a02ad2609/content
-        // Equation 11 - DC partial self inductance (How is DC involved in inductance???)
-        const h = via.height;
-        const r1 = via.inner_diameter/2;
-        const r2 = via.outer_diameter/2;
-        const a0 = r2**2 - r1**2;
-        return h*mu_0/(2*Math.PI)*((r1**4)/(a0**2)*Math.log(r2/r1) + (3*r1**2 - r2**2)/(4*a0));
-      }
-      case 3: {
-        // https://upcommons.upc.edu/server/api/core/bitstreams/928a71c0-b0a3-49ad-bf73-6f7a02ad2609/content
-        // Equation 9 - Wu
-        const h = via.height;
-        const r = via.outer_diameter/2;
-        const a0 = Math.sqrt(1 + h**2/r**2);
-        const a1 = Math.sqrt(1 + r**2/h**2);
-        return mu_0/(2*Math.PI)*(Math.log(1/r+a0) - a1 + r/h + 1/4);
-      }
-      case 4: {
-        // https://upcommons.upc.edu/server/api/core/bitstreams/928a71c0-b0a3-49ad-bf73-6f7a02ad2609/content
-        // Equation 13 - Thin wall approximation
-        const h = via.height;
-        const r1 = via.inner_diameter/2;
-        const r2 = via.outer_diameter/2;
-        const a0 = r1/r2;
-        const a1 = a0**2;
-        const R = Math.max(h,r1,r2)*10; // some wierd virtual cylinder we need???
-        return h*mu_0/(4*Math.PI)*((1-a1+2*a1*Math.log(a0))/(1-a1) + 2*Math.log(R/r2));
-      }
-      case 5: {
-        // dodgy approximation which converts to distributed capacitance and inductance (along height of via)
-        const h = via.height;
-        const C0 = Ch/h;
-        const L0 = 1/(c_0**2 * C0);
-        return L0*h;
-      }
-    }
-  };
-
-  const Lh = calculate_inductance(2);
+  let Lh = undefined as (number | undefined);
+  if (via.inner_diameter/via.outer_diameter < 0.05) {
+    // solid core approximation
+    Lh = calculate_via_inductance(via, "solid_wu");
+  } else if (via.inner_diameter/via.outer_diameter > 0.95) {
+    // thin wall approximation
+    Lh = calculate_via_inductance(via, "tube_thin_sapongin_prokopenko");
+  } else {
+    // tube approximation
+    Lh = calculate_via_inductance(via, "tube_sapongin_prokopenko");
+  }
   // round trip inductance
   // M1. assume copper plane doubles it
   // M2. assuume copper plane has negligible inductance???
