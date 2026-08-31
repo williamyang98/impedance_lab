7cd48d324384e0c5ed6abe1a225b3677eb6868bf
William Yang <williamyang2017@gmail.com>
2026-08-31 14:06:40 +1000

    Estimate impedance of setups

--- src/views/app_3d/app_3d.ts
+++ src/views/app_3d/app_3d.ts
@@ -1,6 +1,25 @@
 import { SimulationSetup, type SimulationSource } from "./grid.ts";
 import { type Size3D } from "../../wgpu_kernels/fdtd_3d/index.ts";
 
+function estimate_single_ended_impedance_ipc2141(er: number, h: number, w: number, t: number): number {
+  // https://www.digikey.com.au/en/resources/conversion-calculators/conversion-calculator-pcb-trace-impedance
+  // er = relative dielectric constant
+  // h = height between trace and plane
+  // w = trace width
+  // t = trace height
+  return 87/Math.sqrt(er+1.41)*Math.log(5.98*h/(0.8*w+t));
+}
+
+function estimate_differential_impedance_ipc2141(er: number, h: number, w: number, t: number, s: number): number {
+  // https://www.digikey.com.au/en/resources/conversion-calculators/conversion-calculator-pcb-trace-impedance
+  // er = relative dielectric constant
+  // h = height between trace and plane
+  // w = trace width
+  // t = trace height
+  // s = trace spacing
+  return 174/Math.sqrt(er+1.41)*Math.log(5.98*h/(0.8*w+t))*(1-0.48*Math.exp(-0.96*s/h));
+}
+
 export function create_single_ended_setup(adapter: GPUAdapter, device: GPUDevice): SimulationSetup {
   const size: Size3D = { x: 256, y: 128, z: 16 };
   const setup = new SimulationSetup(adapter, device, size);
@@ -19,9 +38,11 @@ export function create_single_ended_setup(adapter: GPUAdapter, device: GPUDevice
   const plane_height = 1;
   const plane_border = 20;
   const signal_height = 1;
-  // let signal_width = 10;
   const signal_width = 20;
-  const separation_height = 5;
+  const separation_height = 10;
+  const dielectric_constant = 4.1;
+  const terminator_thickness = 2;
+
   const z_start = Math.floor(Nz/2 - (plane_height+separation_height+signal_height)/2);
   // ground plane
   cpu.sigma_k
@@ -32,7 +53,7 @@ export function create_single_ended_setup(adapter: GPUAdapter, device: GPUDevice
   cpu.epsilon_r
     .lo([z_start+plane_height, plane_border, plane_border])
     .hi([separation_height, Ny-plane_border*2, Nx-plane_border*2])
-    .fill(4.1);
+    .fill(dielectric_constant);
   // single ended transmission line
   cpu.sigma_k
     .lo([z_start+plane_height+separation_height, Math.floor(Ny/2-signal_width/2), plane_border])
@@ -57,19 +78,17 @@ export function create_single_ended_setup(adapter: GPUAdapter, device: GPUDevice
 
   // terminator resistors
   {
-    // const resistance = 78.338/2; // w=10
-    const resistance = 53.864/2; // w=20
-    const thickness = 1;
-    const area = (signal_width*dxyz)*(thickness*dxyz);
+    const resistance = estimate_single_ended_impedance_ipc2141(dielectric_constant, separation_height, signal_width, signal_height);
+    const area = (signal_width*dxyz)*(terminator_thickness*dxyz);
     const length = separation_height*dxyz;
     const sigma = length/(resistance*area);
     cpu.sigma_k
       .lo([z_start+plane_height,Math.floor(Ny/2-signal_width/2),plane_border])
-      .hi([separation_height,signal_width,thickness])
+      .hi([separation_height,signal_width,terminator_thickness])
       .fill(sigma);
     cpu.sigma_k
-      .lo([z_start+plane_height, Math.floor(Ny/2-signal_width/2), Nx-plane_border-thickness])
-      .hi([separation_height, signal_width, thickness])
+      .lo([z_start+plane_height, Math.floor(Ny/2-signal_width/2), Nx-plane_border-terminator_thickness])
+      .hi([separation_height, signal_width, terminator_thickness])
       .fill(sigma);
   }
   cpu.bake_materials();
@@ -100,6 +119,8 @@ export function create_differential_setup(adapter: GPUAdapter, device: GPUDevice
   const signal_width = 20;
   const signal_spacing = 15;
   const separation_height = 10;
+  const dielectric_constant = 4.1;
+  const terminator_thickness = 2;
 
   const z_start = Math.floor(Nz/2 - (plane_height+separation_height+signal_height)/2);
   // ground plane
@@ -111,7 +132,7 @@ export function create_differential_setup(adapter: GPUAdapter, device: GPUDevice
   cpu.epsilon_r
     .lo([z_start+plane_height, plane_border, plane_border])
     .hi([separation_height, Ny-plane_border*2, Nx-plane_border*2])
-    .fill(4.1);
+    .fill(dielectric_constant);
   // differential transmission lines
   cpu.sigma_k
     .lo([z_start+plane_height+separation_height, Math.floor(Ny/2-signal_spacing/2-signal_width), plane_border])
@@ -143,22 +164,20 @@ export function create_differential_setup(adapter: GPUAdapter, device: GPUDevice
 
   // terminator resistors
   {
-    // const resistance = 78.338/2; // w=10
-    const resistance = 53.864/2; // w=20
-    const thickness = 1;
-    const area = (signal_width*dxyz)*(thickness*dxyz);
+    const resistance = estimate_differential_impedance_ipc2141(dielectric_constant, separation_height, signal_width, signal_height, signal_spacing);
+    const area = (signal_width*dxyz)*(terminator_thickness*dxyz);
     const length = separation_height*dxyz;
     const sigma = length/(resistance*area);
     const add_terminator = (x: number, y: number) => {
       cpu.sigma_k
         .lo([z_start+plane_height,y,x])
-        .hi([separation_height,signal_width,thickness])
+        .hi([separation_height,signal_width,terminator_thickness])
         .fill(sigma);
     };
     add_terminator(plane_border, Math.floor(Ny/2-signal_spacing/2-signal_width));
     add_terminator(plane_border, Math.floor(Ny/2+signal_spacing/2));
-    add_terminator(Nx-plane_border-thickness, Math.floor(Ny/2-signal_spacing/2-signal_width));
-    add_terminator(Nx-plane_border-thickness, Math.floor(Ny/2+signal_spacing/2));
+    add_terminator(Nx-plane_border-terminator_thickness, Math.floor(Ny/2-signal_spacing/2-signal_width));
+    add_terminator(Nx-plane_border-terminator_thickness, Math.floor(Ny/2+signal_spacing/2));
   }
   cpu.bake_materials();
 
